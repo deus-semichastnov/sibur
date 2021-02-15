d0988fc0cc21a6b7b2270858e23fe80d808d256d
e.semichastnov <e.semichastnov@web-hands.ru>
2021-02-15 16:55:20 +0300

    init

---
+++ bitrix/templates/sibur-landing/include/footer/social.php
@@ -0,0 +1,25 @@
+<div class="footer__social">
+    Подписывайтесь
+    <div class="footer__social-wrap">
+        <a href="#" class="footer__social-item">
+            <svg>
+                <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprite-social.svg#social-fb"></use>
+            </svg>
+        </a>
+        <a href="#" class="footer__social-item">
+            <svg>
+                <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprite-social.svg#social-vk"></use>
+            </svg>
+        </a>
+        <a href="#" class="footer__social-item">
+            <svg>
+                <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprite-social.svg#social-insta"></use>
+            </svg>
+        </a>
+        <a href="#" class="footer__social-item">
+            <svg>
+                <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprite-social.svg#social-odnok"></use>
+            </svg>
+        </a>
+    </div>
+</div>
