6ea51411c52fd193468edc1e34686127a57a6a80
e.semichastnov <e.semichastnov@web-hands.ru>
2021-02-15 16:57:49 +0300

    init

---
+++ bitrix/templates/sibur-landing/include/footer/footer-desc.php
@@ -0,0 +1,9 @@
+<div class="footer__cookie">Для повышения удобства работы с сайтом СИБУР Диджитал использует файлы
+    cookie. В cookie содержатся данные о прошлых посещениях сайта. Если вы не хотите, чтобы эти данные
+    обрабатывались, отключите cookie в настройках браузера</div>
+<a href="https://de-us.ru/" class="footer__by" target="_blank">
+    design by
+    <svg>
+        <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprite-other.svg#deus-logo"></use>
+    </svg>
+</a>
