c63e5b3a521004ac6b634109e434f5b12f83c81f
e.semichastnov <e.semichastnov@web-hands.ru>
2021-02-19 16:14:08 +0300

    init

---
+++ bitrix/templates/sibur-landing/include/main/empty_cases.php
@@ -0,0 +1,34 @@
+<section class="cases --offer" id="cases">
+    <div class="container">
+        <div class="cases__wrap">
+            <div class="cases__head">
+                <h3 class="heading-tertiary">Для кого</h3>
+                <h2 class="heading-secondary">Какие компетенции мы ищем в первую очередь</h2>
+                <div class="cases__subtitle">В 2017 году СИБУР анонсировал масштабную цифровую трансформацию
+                    – повышение эффективности за счёт внедрения новых технологий по всей цепочке создания
+                    ценности» </div>
+            </div>
+            <div class="cases__content">
+                <div class="cases__info">
+                    <div class="cases__info-text">Здесь мы создаём и внедряем новые цифровые продукты, которые
+                        позволяют оптимизировать бизнес-процессы, а также повысить уровень эффективности
+                        компании</div>
+                </div>
+                <div class="cases__c">
+                    <div class="cases__item">
+                        <div class="cases__item-text">У Вас есть идеи или новый продукт? Подайте заявку уже
+                            сейчас и мы обязательно с Вами свяжемся</div>
+                        <a href="#quest" class="btn --white fancybox-link">
+                            Подать заявку
+                            <div class="btn__plus">
+                                <div class="btn__plus-round"></div>
+                                <div class="btn__plus-p"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <h4 class="heading-quaternary">• разработки</h4>
+        </div>
+    </div>
+</section>
