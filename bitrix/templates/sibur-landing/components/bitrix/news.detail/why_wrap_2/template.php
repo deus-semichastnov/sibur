05fc5f23f56093195bc56e3e2fd61bf7fb547207
e.semichastnov <e.semichastnov@web-hands.ru>
2021-02-19 14:49:38 +0300

    template why_wrap_2

--- bitrix/templates/sibur-landing/components/bitrix/news.detail/why_wrap_2/template.php
+++ bitrix/templates/sibur-landing/components/bitrix/news.detail/why_wrap_2/template.php
@@ -2,46 +2,38 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)
     die();
 ?>
-<?//echo "<pre>"; print_r($arResult); echo "</pre>";?>
+
 <? if (!empty($arResult)): ?>
     <div class="why__offer">
         <div class="why__offer-wrap">
             <div class="why__offer-title">
-                <h3 class="heading-tertiary">Уже есть, готовые проекты?</h3>
-                <h2 class="heading-secondary">Вы можете предложить </h2>
+                <h3 class="heading-tertiary"><?=$arResult["NAME"]?></h3>
+                <h2 class="heading-secondary"><?=$arResult["PROPERTIES"]["TITLE_BLOCK"]["VALUE"]?></h2>
             </div>
-            <div class="why__offer-content">
-                <div class="why__offer-item active increase__item">
-                    <svg>
-                        <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprite-main.svg#why-icon-1"></use>
-                    </svg>
-                    <div class="why__offer-descr">Цифровое решение для конкретных бизнес-задач
-                    </div>
-                    <a href="#quest" class="btn --white fancybox-link">
-                        Предложить
-                        <div class="btn__plus">
-                            <div class="btn__plus-round"></div>
-                            <div class="btn__plus-p"></div>
+            <?if($arResult["PROPERTIES"]["SLIDER"]["VALUE"]):?>
+                <?$arSlides = getSlides($arResult["PROPERTIES"]["SLIDER"]["VALUE"]);?>
+                <div class="why__offer-content">
+                    <?foreach ($arSlides as $slide):?>
+                        <?$active = "";?>
+                        <?if($slide["PROPERTY_ACTIVE_SLIDE_VALUE"]){
+                            $active = " active";
+                        }?>
+                        <?//echo "<pre>"; print_r($slide); echo "</pre>";?>
+                        <div class="why__offer-item<?=$active?> increase__item">
+                            <img src="<?=$slide["PIC_SRC"]?>" alt="<?=$slide["NAME"]?>">
+                            <div class="why__offer-descr"><?=$slide["PREVIEW_TEXT"]?></div>
+                            <a href="<?=$slide["PROPERTY_LINK_VALUE"]?>" class="btn --white fancybox-link">
+                                <?=$slide["PROPERTY_TEXT_BTN_VALUE"]?>
+                                <div class="btn__plus">
+                                    <div class="btn__plus-round"></div>
+                                    <div class="btn__plus-p"></div>
+                                </div>
+                            </a>
                         </div>
-                    </a>
+                    <?endforeach;?>
                 </div>
-                <div class="why__offer-item increase__item">
-                    <svg>
-                        <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprite-main.svg#why-icon-2"></use>
-                    </svg>
-                    <div class="why__offer-descr">Представить собственную технологию по другим
-                        тематикам
-                    </div>
-                    <a href="#quest" class="btn --white fancybox-link">
-                        Предложить
-                        <div class="btn__plus">
-                            <div class="btn__plus-round"></div>
-                            <div class="btn__plus-p"></div>
-                        </div>
-                    </a>
-                </div>
-            </div>
+            <?endif;?>
         </div>
-        <h4 class="heading-quaternary">• 5 причин подать заявку</h4>
+        <h4 class="heading-quaternary"><?=$arResult["PROPERTIES"]["NEXT_BLOCK"]["VALUE"]?></h4>
     </div>
 <? endif ?>
