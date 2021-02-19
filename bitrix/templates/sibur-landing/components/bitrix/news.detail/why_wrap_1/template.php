8d85695a348a69989117488601f9cd9b71b41edf
e.semichastnov <e.semichastnov@web-hands.ru>
2021-02-19 14:41:15 +0300

    template why_wrap_1

--- bitrix/templates/sibur-landing/components/bitrix/news.detail/why_wrap_1/template.php
+++ bitrix/templates/sibur-landing/components/bitrix/news.detail/why_wrap_1/template.php
@@ -2,60 +2,47 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)
     die();
 ?>
-<?//echo "<pre>"; print_r($arResult); echo "</pre>";?>
+
 <? if (!empty($arResult)): ?>
-    <h3 class="heading-tertiary">Зачем</h3>
-    <h2 class="heading-secondary">Почему СИБУР нуждается в вашем предложении?</h2>
-    <div class="why__picture">
-        <div class="why__picture-img --bg">
-            <img src="<?=SITE_TEMPLATE_PATH?>/img/why-1.png" alt="Зачем">
-        </div>
-        <div class="why__picture-img rellax --anim">
-            <img src="<?=SITE_TEMPLATE_PATH?>/img/why-2.png" alt="Почему СИБУР нуждается в вашем предложении?">
-        </div>
-    </div>
-    <div class="why__tabs">
-        <div class="why__tabs-info">
-            <div class="why__tabs-btn active">
-                <div class="why__tabs-checkbox"></div>
-                Технологически вызовы
+    <h3 class="heading-tertiary"><?=$arResult["NAME"]?></h3>
+    <h2 class="heading-secondary"><?=$arResult["PROPERTIES"]["TITLE_BLOCK"]["VALUE"]?></h2>
+    <?if($arResult["PROPERTIES"]["IMG_BLOCK"]["VALUE"]):?>
+        <div class="why__picture">
+            <div class="why__picture-img --bg">
+                <img src="<?=$arResult["DISPLAY_PROPERTIES"]["IMG_BLOCK"]["FILE_VALUE"][0]["SRC"]?>" alt="<?=$arResult["DISPLAY_PROPERTIES"]["IMG_BLOCK"]["FILE_VALUE"][0]["DESCRIPTION"]?>">
             </div>
-            <div class="why__tabs-btn">
-                <div class="why__tabs-checkbox"></div>
-                Культура компании
-            </div>
-            <div class="why__tabs-btn">
-                <div class="why__tabs-checkbox"></div>
-                Фокус
+            <div class="why__picture-img rellax --anim">
+                <img src="<?=$arResult["DISPLAY_PROPERTIES"]["IMG_BLOCK"]["FILE_VALUE"][1]["SRC"]?>" alt="<?=$arResult["DISPLAY_PROPERTIES"]["IMG_BLOCK"]["FILE_VALUE"][1]["DESCRIPTION"]?>">
             </div>
         </div>
-        <div class="why__tabs-content show">
-            <svg>
-                <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprite-main.svg#tabs-icon-1"></use>
-            </svg>
-            <div class="why__tabs-text">У нас много актуальных технологических вызовов, которые
-                требуют специфичных и кастомных решений
+    <?endif;?>
+    <?if($arResult["PROPERTIES"]["SLIDER"]["VALUE"]):?>
+        <?$arSlides = getSlides($arResult["PROPERTIES"]["SLIDER"]["VALUE"]);?>
+        <?//echo "<pre>"; print_r($arSlides); echo "</pre>";?>
+        <div class="why__tabs">
+            <div class="why__tabs-info">
+                <?foreach ($arSlides as $slide):?>
+                    <?$active = "";?>
+                    <?if($slide["PROPERTY_ACTIVE_SLIDE_VALUE"]){
+                        $active = " active";
+                    }?>
+                    <div class="why__tabs-btn<?=$active?>">
+                        <div class="why__tabs-checkbox"></div>
+                        <?=$slide["NAME"]?>
+                    </div>
+                <?endforeach;?>
             </div>
+
+            <?foreach ($arSlides as $slide):?>
+                <?$show = "";?>
+                <?if($slide["PROPERTY_ACTIVE_SLIDE_VALUE"]){
+                    $show = " show";
+                }?>
+                <div class="why__tabs-content<?=$show?>">
+                    <img src="<?=$slide["PIC_SRC"]?>" alt="tabs">
+                    <div class="why__tabs-text"><?=$slide["PREVIEW_TEXT"]?></div>
+                </div>
+            <?endforeach;?>
         </div>
-        <div class="why__tabs-content">
-            <svg>
-                <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprite-main.svg#tabs-icon-1"></use>
-            </svg>
-            <div class="why__tabs-text">Уникальные и нестандартные задачи решаются с помощью передовых
-                технологий, неожиданных идей и талантов, которые объединяются здесь, в СИБУРЕ. Мы
-                нацелены на раскрытие и объединении всех участников, ведь именно так возможно решить
-                самые неординарные задачи.
-            </div>
-        </div>
-        <div class="why__tabs-content">
-            <svg>
-                <use xlink:href="<?=SITE_TEMPLATE_PATH?>/img/sprite-main.svg#tabs-icon-1"></use>
-            </svg>
-            <div class="why__tabs-text">Новаторские подходы, передовые технологии. Все это мы используем
-                для достижения максимального результата. Мы не останавливаемся на достигнутом, каждый
-                раз находимся в поиске свежих идей и знаний, совершенствуем технологии и повышаем
-                эффективность работы компаний
-            </div>
-        </div>
-    </div>
+    <?endif;?>
 <? endif ?>
