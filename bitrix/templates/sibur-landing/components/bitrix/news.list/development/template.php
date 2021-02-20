02995d778ec8ccf50edcb46d9d31199a9b080ba8
e.semichastnov <e.semichastnov@web-hands.ru>
2021-02-20 09:49:10 +0300

    news.list develop template

--- bitrix/templates/sibur-landing/components/bitrix/news.list/development/template.php
+++ bitrix/templates/sibur-landing/components/bitrix/news.list/development/template.php
@@ -2,145 +2,34 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)
     die();
 ?>
-<? if (!empty($arResult)): ?>
+<? if (!empty($arResult["ITEMS"])): ?>
+<?//echo "<pre>"; print_r($arResult["ITEMS"]); echo "</pre>";?>
     <div class="develop__content">
-        <div class="develop__item">
-            <div class="develop__num">01</div>
-            <div class="develop__picture">
-                <img src="<?=SITE_TEMPLATE_PATH?>/img/develop/develop-1.png" alt="Система виброконтроля EXV-01">
-            </div>
-            <div class="develop__text">
-                <div class="heading-tertiary">IIoT датчики</div>
-                <div class="develop__name">Система виброконтроля EXV-01</div>
-                <div class="develop__text-descr">Уникальное IIoT-устройство для измерения показаний СКЗ
-                    виброскорости в мм/сек и температуры в градусах Цельсия в условиях работы во
-                    взрывоопасных средах с последующей передачей полученных значений по беспроводному
-                    протоколу LoRaWAN.</div>
-                <div class="btn --green" data-link="panel--1">
-                    Все характеристи
-                    <div class="btn__plus">
-                        <div class="btn__plus-round"></div>
-                        <div class="btn__plus-p"></div>
+        <?foreach ($arResult["ITEMS"] as $key => $item):?>
+            <?$revers = "";
+            $num = $key+1;
+            if($num%2){
+                $revers = " --reverse";
+            }
+            $numStr = str_pad($num, 2, "0", STR_PAD_LEFT);?>
+            <div class="develop__item<?=$revers?>">
+                <div class="develop__num"><?=$numStr?></div>
+                <div class="develop__picture">
+                    <img src="<?=$item["PREVIEW_PICTURE"]["SRC"]?>" alt="<?=$item["PREVIEW_PICTURE"]["TITLE"]?>">
+                </div>
+                <div class="develop__text">
+                    <div class="heading-tertiary"><?=$item["NAME"]?></div>
+                    <div class="develop__name"><?=$item["PROPERTIES"]["TITLE"]["VALUE"]?></div>
+                    <div class="develop__text-descr"><?=$item["PREVIEW_TEXT"]?></div>
+                    <div class="btn --green" data-link="panel--<?=$item["ID"]?>" data-panel-id="<?=$item["ID"]?>">
+                        Все характеристи
+                        <div class="btn__plus">
+                            <div class="btn__plus-round"></div>
+                            <div class="btn__plus-p"></div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="develop__item --reverse">
-            <div class="develop__num">02</div>
-
-            <div class="develop__picture">
-                <img src="<?=SITE_TEMPLATE_PATH?>/img/develop/develop-2.png" alt="Датчик контроля температуры
-                                EXT-01">
-            </div>
-            <div class="develop__text">
-                <div class="heading-tertiary">IIoT датчики</div>
-                <div class="develop__name">Датчик контроля температуры
-                    EXT-01</div>
-                <div class="develop__text-descr">Уникальное IIoT-устройство для контроля температуры
-                    агрегатов в суровых климатических условиях на предприятиях нефтегазового сегмента и
-                    любых других производственных площадках. Показания температуры, измеряемые выносным
-                    сенсором различной формы, передаются по протоколу LoRaWAN с настраиваемым периодом
-                    отправки сообщений.</div>
-                <div class="btn --green" data-link="panel--2">
-                    Все характеристи
-                    <div class="btn__plus">
-                        <div class="btn__plus-round"></div>
-                        <div class="btn__plus-p"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="develop__item">
-            <div class="develop__num">03</div>
-            <div class="develop__picture">
-                <img src="<?=SITE_TEMPLATE_PATH?>/img/develop/develop-3.png" alt="IBEACON EXB-01">
-            </div>
-            <div class="develop__text">
-                <div class="heading-tertiary">IIoT датчики</div>
-                <div class="develop__name">IBEACON EXB-01</div>
-                <div class="develop__text-descr">Уникальное IIoT-устройство для системы внутренней навигации
-                    персонала на производственных участках с отсутствием покрытия GPS. Радиомаяк
-                    осуществляет периодическую широковещательную рассылку сигналов, содержащих
-                    идентифицирующую информацию, на основе которой вычисляется положение подвижного объекта.
-                </div>
-                <div class="btn --green" data-link="panel--3">
-                    Все характеристи
-                    <div class="btn__plus">
-                        <div class="btn__plus-round"></div>
-                        <div class="btn__plus-p"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="develop__item --reverse">
-            <div class="develop__num">04</div>
-            <div class="develop__picture">
-                <img src="<?=SITE_TEMPLATE_PATH?>/img/develop/develop-4.png" alt="Платформа Интернет Вещей">
-            </div>
-            <div class="develop__text">
-                <div class="heading-tertiary">IIoT платформа</div>
-                <div class="develop__name">Платформа Интернет Вещей</div>
-                <div class="develop__text-descr">• Быстрое подключение LoRaWAN-устройств к сети <br>
-                    • Удобное размещение датчиков на мнемосхемах <br>
-                    • Визуализация показаний на графиках <br>
-                    • Список событий и звуковые оповещения</div>
-                <div class="btn --green" data-link="panel--4">
-                    Все характеристи
-                    <div class="btn__plus">
-                        <div class="btn__plus-round"></div>
-                        <div class="btn__plus-p"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="develop__item">
-            <div class="develop__num">05</div>
-            <div class="develop__picture">
-                <img src="<?=SITE_TEMPLATE_PATH?>/img/develop/develop-5.png" alt="УДАЛЕННЫЙ ЭКСПЕРТ AR">
-            </div>
-            <div class="develop__text">
-                <div class="heading-tertiary">платформа AR</div>
-                <div class="develop__name">УДАЛЕННЫЙ ЭКСПЕРТ AR</div>
-                <div class="develop__text-descr">В 2020 году СИБУР разработал и внедрил собственное решение
-                    УДАЛЕННЫЙ ЭКСПЕРТ AR, сделав акцент на:
-                    <br>
-                    <br>
-                    • Быстрое привлечение внутренних и внешних
-                    экспертов<br>
-                    • Соблюдение требований безопасности
-                    проведения работ, в т.ч. во взрывоопасных
-                    помещениях<br>
-                    • Один сервис – множество сценариев
-                    применения</div>
-                <div class="btn --green" data-link="panel--5">
-                    Все характеристи
-                    <div class="btn__plus">
-                        <div class="btn__plus-round"></div>
-                        <div class="btn__plus-p"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="develop__item --reverse">
-            <div class="develop__num">06</div>
-            <div class="develop__picture">
-                <img src="<?=SITE_TEMPLATE_PATH?>/img/develop/develop-6.png" alt="Система виброконтроля EXV-01">
-            </div>
-            <div class="develop__text">
-                <div class="heading-tertiary">промышленный смартфон</div>
-                <div class="develop__name">Смартфон SIBUR EX50</div>
-                <div class="develop__text-descr">зрывобезопасный смартфон SIBUR EX50 создан для
-                    использования в индустриальных условиях с повышенным риском: Нефтегазового предприятия,
-                    Угледобывающая промышленность, Химические предприятия, Морские перевозки опасных веществ
-                </div>
-                <div class="btn --green" data-link="panel--6">
-                    Все характеристи
-                    <div class="btn__plus">
-                        <div class="btn__plus-round"></div>
-                        <div class="btn__plus-p"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?endforeach?>
     </div>
 <? endif ?>
